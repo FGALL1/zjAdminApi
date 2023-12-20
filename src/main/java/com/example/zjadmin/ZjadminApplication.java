f669c142c437d48d7e774acf3b206a8d13327205
FGALL1 <2297445147@qq.com>
2023-12-21 00:11:50 +0800

    init

---
+++ src/main/java/com/example/zjadmin/ZjadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.zjadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZjadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zjadmin/ZjadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zjadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
